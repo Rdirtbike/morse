ecd5570aef10b0f7c46678f9ea2c0324b13cbc74
Ryan Meredith <ryan@affiniteinstruments.com>
2024-01-17 11:41:50 -0500

    Added esp32c3 support

---
+++ esp32c3-morse/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use common::{flash_from_channel, run_queue};
+use embassy_executor::{main, Spawner};
+use embassy_futures::join::join;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
+use esp32c3_hal::{
+    clock::ClockControl, embassy::init, peripherals::Peripherals, system::SystemExt,
+    timer::TimerGroup, IO,
+};
+use panic_halt as _;
+
+#[main(entry = "esp32c3_hal::entry")]
+async fn main(_spawner: Spawner) {
+    let peripherals = Peripherals::take();
+    let clocks = ClockControl::boot_defaults(peripherals.SYSTEM.split().clock_control).freeze();
+    let queue: Channel<NoopRawMutex, _, 100> = Channel::new();
+    init(&clocks, TimerGroup::new(peripherals.TIMG0, &clocks).timer0);
+    join(
+        run_queue(&queue),
+        flash_from_channel(
+            &queue,
+            IO::new(peripherals.GPIO, peripherals.IO_MUX)
+                .pins
+                .gpio7
+                .into_push_pull_output(),
+        ),
+    )
+    .await;
+}
