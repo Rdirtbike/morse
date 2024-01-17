f84328507dfdf2751f714193915e505428b1eb94
Ryan Meredith <ryan@affiniteinstruments.com>
2024-01-17 13:56:40 -0500

    Accept messages over usb

--- esp32c3-morse/src/main.rs
+++ esp32c3-morse/src/main.rs
@@ -2,13 +2,14 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use common::{flash_from_channel, run_queue};
+use common::{flash_from_channel, Queue};
 use embassy_executor::{main, Spawner};
 use embassy_futures::join::join;
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
+use embedded_io_async::Read;
 use esp32c3_hal::{
     clock::ClockControl, embassy::init, peripherals::Peripherals, system::SystemExt,
-    timer::TimerGroup, IO,
+    timer::TimerGroup, UsbSerialJtag, IO,
 };
 use panic_halt as _;
 
@@ -19,7 +20,14 @@ async fn main(_spawner: Spawner) {
     let queue: Channel<NoopRawMutex, _, 100> = Channel::new();
     init(&clocks, TimerGroup::new(peripherals.TIMG0, &clocks).timer0);
     join(
-        run_queue(&queue),
+        async {
+            let mut usb = UsbSerialJtag::new(peripherals.USB_DEVICE);
+            let mut buf = [b'0'; 25];
+            loop {
+                let n = usb.read(&mut buf).await.unwrap_or_else(|x| match x {});
+                (&queue).queue_string(&buf[..n]).await;
+            }
+        },
         flash_from_channel(
             &queue,
             IO::new(peripherals.GPIO, peripherals.IO_MUX)
