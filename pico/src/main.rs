b5db9c893a59d53f8dc3e4ade525c35875d8e469
Ryan Meredith <ryan@affiniteinstruments.com>
2024-01-22 11:32:51 -0500

    Use second core

--- pico/src/main.rs
+++ pico/src/main.rs
@@ -2,24 +2,36 @@
 #![no_std]
 #![no_main]
 
-use common::{flash_from_channel, run_queue};
-use embassy_executor::{main, Spawner};
-use embassy_futures::join::join;
+use common::{flash_from_channel, run_queue, MorseCode};
+use embassy_executor::{main, task, Executor, Spawner};
 use embassy_rp::{
     config::Config,
     gpio::{Level, Output},
     init,
+    multicore::{spawn_core1, Stack},
+    peripherals::PIN_25,
 };
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 use panic_halt as _;
+use static_cell::make_static;
+
+static QUEUE: Channel<CriticalSectionRawMutex, MorseCode, 100> = Channel::new();
 
 #[main]
 async fn main(_spawner: Spawner) {
     let peripherals = init(Config::default());
-    let queue: Channel<NoopRawMutex, _, 100> = Channel::new();
-    join(
-        run_queue(&queue),
-        flash_from_channel(&queue, Output::new(peripherals.PIN_25, Level::Low)),
-    )
-    .await;
+    spawn_core1(
+        peripherals.CORE1,
+        make_static!(Stack::<4096>::new()),
+        || {
+            make_static!(Executor::new())
+                .run(|s| s.must_spawn(test(Output::new(peripherals.PIN_25, Level::Low))))
+        },
+    );
+    run_queue(&QUEUE).await
+}
+
+#[task]
+async fn test(pin: Output<'static, PIN_25>) {
+    flash_from_channel(&QUEUE, pin).await
 }
