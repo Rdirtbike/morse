b15226dba3f0bf8079f8ec6b80249827078c3570
Ryan Meredith <ryan@affiniteinstruments.com>
2024-01-22 13:54:00 -0500

    Accept messages over usb on pico

--- pico/src/main.rs
+++ pico/src/main.rs
@@ -2,24 +2,102 @@
 #![no_std]
 #![no_main]
 
-use common::{flash_from_channel, run_queue};
-use embassy_executor::{main, Spawner};
+use common::{flash_from_channel, read_and_queue};
+use embassy_executor::{main, task, Spawner};
 use embassy_futures::join::join;
 use embassy_rp::{
+    bind_interrupts,
     config::Config,
     gpio::{Level, Output},
     init,
+    peripherals::USB,
+    usb::{Driver, InterruptHandler},
 };
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
+use embassy_usb::{
+    class::cdc_acm::{CdcAcmClass, State},
+    driver::EndpointError,
+    Builder, UsbDevice,
+};
+use embedded_io_async::{Error, ErrorKind, ErrorType, Read};
 use panic_halt as _;
+use static_cell::make_static;
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
 
 #[main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let peripherals = init(Config::default());
     let queue: Channel<NoopRawMutex, _, 100> = Channel::new();
     join(
-        run_queue(&queue),
+        read_and_queue(
+            &queue,
+            Usb::new(Driver::new(peripherals.USB, Irqs), spawner),
+        ),
         flash_from_channel(&queue, Output::new(peripherals.PIN_25, Level::Low)),
     )
     .await;
+}
+
+struct Usb {
+    serial: CdcAcmClass<'static, Driver<'static, USB>>,
+}
+
+impl Usb {
+    const CONFIG: embassy_usb::Config<'static> = {
+        let mut config = embassy_usb::Config::new(0x2E8A, 0x000A);
+        config.manufacturer = Some("Ryan Meredith");
+        config.product = Some("Morse Code Flasher");
+        config.serial_number = Some("1");
+        config.max_packet_size_0 = 64;
+        config.device_class = 0xEF;
+        config.device_sub_class = 0x02;
+        config.device_protocol = 0x01;
+        config.composite_with_iads = true;
+        config
+    };
+
+    fn new(driver: Driver<'static, USB>, spawner: Spawner) -> Self {
+        let mut builder = Builder::new(
+            driver,
+            Self::CONFIG,
+            make_static!([0; 256]),
+            make_static!([0; 256]),
+            make_static!([0; 256]),
+            make_static!([0; 256]),
+            make_static!([0; 64]),
+        );
+        let serial = CdcAcmClass::new(&mut builder, make_static!(State::new()), 64);
+        spawner.must_spawn(run_usb(builder.build()));
+        Self { serial }
+    }
+}
+
+impl ErrorType for Usb {
+    type Error = MyError;
+}
+
+impl Read for Usb {
+    async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+        self.serial.read_packet(buf).await.map_err(MyError)
+    }
+}
+
+#[derive(Debug)]
+struct MyError(EndpointError);
+
+impl Error for MyError {
+    fn kind(&self) -> embedded_io_async::ErrorKind {
+        match self.0 {
+            EndpointError::BufferOverflow => ErrorKind::OutOfMemory,
+            EndpointError::Disabled => ErrorKind::NotConnected,
+        }
+    }
+}
+
+#[task]
+async fn run_usb(mut usb: UsbDevice<'static, Driver<'static, USB>>) -> ! {
+    usb.run().await
 }
