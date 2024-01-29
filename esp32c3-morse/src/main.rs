a5325b593ed9c2bb524d3f2243901970e1c527de
Ryan Meredith <ryan@affiniteinstruments.com>
2024-01-29 12:33:07 -0500

    Changed panic handler

--- esp32c3-morse/src/main.rs
+++ esp32c3-morse/src/main.rs
@@ -2,8 +2,6 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use core::pin::Pin;
-
 use common::{flash_from_channel, read_and_queue, MorseCode};
 use embassy_executor::{task, Spawner};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
@@ -11,17 +9,16 @@ use esp32c3_hal::{
     clock::ClockControl, embassy::init, entry, macros::main, peripherals::Peripherals,
     system::SystemExt, timer::TimerGroup, UsbSerialJtag, IO,
 };
-use panic_write::PanicHandler;
+use esp_backtrace as _;
 
 static QUEUE: Channel<CriticalSectionRawMutex, MorseCode, 100> = Channel::new();
 
 #[main]
 async fn main(spawner: Spawner) -> ! {
     let peripherals = Peripherals::take();
-    let usb = PanicHandler::new(UsbSerialJtag::new(peripherals.USB_DEVICE));
     let clocks = ClockControl::boot_defaults(peripherals.SYSTEM.split().clock_control).freeze();
     init(&clocks, TimerGroup::new(peripherals.TIMG0, &clocks));
-    spawner.must_spawn(queue(usb));
+    spawner.must_spawn(queue(UsbSerialJtag::new(peripherals.USB_DEVICE)));
     flash_from_channel(
         &QUEUE,
         IO::new(peripherals.GPIO, peripherals.IO_MUX)
@@ -33,6 +30,6 @@ async fn main(spawner: Spawner) -> ! {
 }
 
 #[task]
-async fn queue(mut usb: Pin<PanicHandler<UsbSerialJtag<'static>>>) -> ! {
-    read_and_queue(&QUEUE, &mut *usb).await
+async fn queue(usb: UsbSerialJtag<'static>) -> ! {
+    read_and_queue(&QUEUE, usb).await
 }
